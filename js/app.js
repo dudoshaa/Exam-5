0d8d93063cd54560e55d46957ade1bc9ab7b4d51
durdona <durdona>
2025-06-17 10:41:53 +0500

    .

--- js/app.js
+++ js/app.js
@@ -13,11 +13,44 @@ desserts.forEach((dessert) => {
   const dessertTitle = clone.querySelector(".dessert-title");
   const dessertDesc = clone.querySelector(".dessert-desc");
   const dessertPrice = clone.querySelector(".dessert-price");
+  const dessertBtnAdd = clone.querySelector(".dessert-btn-add");
+  const dessertBtn = clone.querySelector(".dessert-btn");
+  const btnRemoveAmount = clone.querySelector(".btn-remove-amount");
+  const amount = clone.querySelector(".amount");
+
+  dessertBtn.addEventListener("click", () => {
+    dessertBtn.classList.add("hidden");
+    dessertBtnAdd.classList.remove("hidden");
+    dessertImage.classList.add("border-red");
+    dessertImage.style.width = "246px";
+  });
+
+  let counter = 1;
+  amount.textContent = counter;
+
+  const btnAddAmount = clone.querySelector(".btn-add-amount");
+
+  btnAddAmount.addEventListener("click", () => {
+    counter++;
+    amount.textContent = counter;
+  });
+
+  btnRemoveAmount.addEventListener("click", () => {
+    counter--;
+    if (counter <= 0) {
+      counter = 0;
+      dessertBtnAdd.classList.add("hidden");
+      dessertBtn.classList.remove("hidden");
+      dessertImage.classList.remove("border-red");
+      dessertImage.style.width = "";
+    } else {
+      amount.textContent = counter;
+    }
+  });
 
   dessertImage.src = images;
   dessertTitle.textContent = title;
   dessertDesc.textContent = description;
   dessertPrice.textContent = formatNumber(price);
-
   dessertsList.appendChild(clone);
 });
